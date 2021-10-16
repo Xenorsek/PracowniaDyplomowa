a0f63eab8ec7ade2c2296abcf22c1fe109bd7e0e
Xenorsek <147616@student.uwm.edu.pl>
2021-10-16 17:04:34 +0200

    Dark Mode

--- music-converter/src/App.js
+++ music-converter/src/App.js
@@ -1,8 +1,20 @@
+import LocaleSwitcher from "./localeSwitcher";
+import { darkTheme, lightTheme, GlobalStyles, useDarkMode, Toggle }from "./toggle-component/Toggle"
+import { ThemeProvider } from 'styled-components';
+
 function App() {
+  const [theme, toggleTheme] = useDarkMode();
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
   return (
+    <ThemeProvider theme={themeMode}>
+      <GlobalStyles />
+      <Toggle theme={theme} toggleTheme={toggleTheme} />
     <div className="App">
-      Hello World
+      <h1>Hello World</h1>
+      <LocaleSwitcher />
     </div>
+    </ThemeProvider>
   );
 }
 
