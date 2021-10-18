01dc18c75dfd138feed8c15ea062cd690a56eb15
Xenorsek <147616@student.uwm.edu.pl>
2021-10-18 19:32:35 +0200

    navbar top

---
+++ music-converter/src/navbar-top/logo/logo.js
@@ -0,0 +1,9 @@
+import logo from './coca-cola-logo.png';
+function Logo() {
+  return (
+    <div>
+      <img src={logo} alt="logo" width="350" height="auto" />
+    </div>
+  );
+}
+export default Logo;
---
+++ music-converter/src/navbar-top/navbar-top-component.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import './navbar-top.css';
+function NavbarTop() {
+  return (
+    <div>
+    </div>
+  );
+}
+export default NavbarTop;
