f9f321c9f3e3ed7eda05a8ce95924d8bc4c64063
Xenorsek <147616@student.uwm.edu.pl>
2021-11-07 15:26:39 +0100

    Added connection to Firebase. Added Authcontext. Added signIn logout hooks.

--- music-converter/src/index.js
+++ music-converter/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './i18n/i18n';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./i18n/i18n";
+import { AuthContextProvider } from "./context/AuthContext";
 ReactDOM.render(
   <BrowserRouter>
-  <React.StrictMode>
+    <React.StrictMode>
+      <AuthContextProvider>
         <App />
-  </React.StrictMode>
+      </AuthContextProvider>
+    </React.StrictMode>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
