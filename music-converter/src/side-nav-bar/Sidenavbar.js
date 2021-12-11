be1a64bae592d019bc5bad8a01e9f1e5139f3c71
Xenorsek <Marcin.koz52@gmail.com>
2021-12-11 15:31:40 +0100

    added sidebardata in sidenavbar cause translation doesnt work in that place

--- music-converter/src/side-nav-bar/Sidenavbar.js
+++ music-converter/src/side-nav-bar/Sidenavbar.js
@@ -2,7 +2,8 @@ import React,{useState} from "react";
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { SidebarData } from "./SidebarData";
+import i18n from "../i18n/i18n";
+import * as IoIcons from "react-icons/io";
 import './Sidebar.css';
 import {IconContext} from'react-icons';
 import { useAuthContext } from "../hooks/useAuthContext";
@@ -10,6 +11,44 @@ function Sidenavbar() {
   const [ sidebar, setSidebar ] = useState(false);
   const {user} = useAuthContext();
   const showSidebar = () => setSidebar(!sidebar);
+
+  const SidebarData = [
+    {
+        title: i18n.t('NavBar.0.Home'),
+        path: '/',
+        icon: <AiIcons.AiFillPlusCircle />,
+        cName: 'nav-text',
+        auth: false,
+    }, 
+    {
+        title: i18n.t('NavBar.1.Library'),
+        path: '/library',
+        icon: <AiIcons.AiFillPlusCircle />,
+        cName: 'nav-text',
+        auth: false,
+    }, 
+    {
+        title: i18n.t('NavBar.2.Upload'),
+        path: '/upload',
+        icon: <AiIcons.AiFillDatabase />,
+        cName: 'nav-text',
+        auth: true,
+    }, 
+    {
+        title: i18n.t('NavBar.3.Uploads'),
+        path: '/uploads',
+        icon: <AiIcons.AiFillStar />,
+        cName: 'nav-text',
+        auth: true,
+    }, 
+    {
+        title: i18n.t('NavBar.4.Favorites'),
+        path: '/favorites',
+        icon: <IoIcons.IoMdThumbsUp />,
+        cName: 'nav-text',
+        auth: true,
+    }, 
+];
   return (
     <div>
         <IconContext.Provider value={{color:'#fff'}}>
