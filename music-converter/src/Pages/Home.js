19d3f5bbcb582b54bde0ffefb532bdcef6ef6396
Xenorsek <Marcin.koz52@gmail.com>
2022-01-23 12:05:43 +0100

    Added some changes in home page

--- music-converter/src/Pages/Home.js
+++ music-converter/src/Pages/Home.js
@@ -1,26 +1,26 @@
 import React from "react"
-
+import {List, ListItem} from '@mui/material';
 class Home extends React.Component{
     render(){
         const { t } = this.props;
         return(
             <div className="Home">
             <h1>{t("Home")}</h1>
-            <span>{t("Welcome")}</span>
+            <span><h3>{t("Welcome")}</h3></span>
             <br></br>
-            <span>Przebieg aplikacji:</span>
+            <span>{t('Introduction.0.ApplicationCourse')}</span>
             <span>
-                <ul>
-                    <li>W pierwszej kolejności załóż konto lub się zaloguj.</li>
-                    <li>Następnie przejdź do upload/konwerter by móc skorzystać z upload'u z pliku bądź z dyktafonu.</li>
-                    <li>Jak uda ci się stworzyć plik zapisz go podając jakiś chwytliwy tytuł.</li>
-                    <li>Następnie by podzielić się nim ze światem upublicznij go w zakładce Twoje konwerty/Your uploads</li>
-                    <li>Gratulacje, teraz mogą zobaczyć je wszyscy w bibliotece.</li>
-                    <li>W bibliotece możesz przesłuchać utworów innych oraz zapisać je do ulubionych klikając serduszko.</li>
-                    <li> Liczba koło serduszek znaczy ile osób je polubiło.</li>
-                </ul>
+                <List>
+                    <ListItem>{t('Introduction.1.LoginOrSignUp')}</ListItem>
+                    <ListItem>{t('Introduction.2.NextUpload')}</ListItem>
+                    <ListItem>{t('Introduction.3.NameConvert')}</ListItem>
+                    <ListItem>{t('Introduction.4.SetAsPublic')}</ListItem>
+                    <ListItem>{t('Introduction.5.Gratz')}</ListItem>
+                    <ListItem>{t('Introduction.6.Library')}</ListItem>
+                    <ListItem>{t('Introduction.7.Favorites')}</ListItem>
+                </List>
                 </span>
-                <span>Baw się dobrze!</span>
+                <span>{t('Introduction.8.HaveFun')}</span>
         </div>
         )
     }
